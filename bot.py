590ea4a772b1cde1ae2d9b02f5222d94c58dedb7
LᴀsᴛDʀᴏɢᴢ <105853088+Lastdrogz@users.noreply.github.com>
2022-06-10 13:04:06 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,84 @@
+
+
+
+
+@Bot.on_callback_query()
+async def button(bot: Client, cmd: CallbackQuery):
+
+    cb_data = cmd.data
+    if "aboutbot" in cb_data:
+        await cmd.message.edit(
+            Config.ABOUT_BOT_TEXT,
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("💸 Dᴏɴᴀᴛᴇ", callback_data="aboutdevs")
+                    ],
+                    [
+                        InlineKeyboardButton("⛺ Hᴏᴍᴇ", callback_data="gotohome"),
+                        InlineKeyboardButton("🗑 Cʟᴏsᴇ", callback_data="closeMessage")
+                    ]
+                ]
+            )
+        )
+
+    elif "aboutdevs" in cb_data:
+        await cmd.message.edit(
+            Config.ABOUT_DEV_TEXT,
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Pᴀʏ 💰 Aᴍᴏᴜɴᴛ",
+                                             url="https://telegram.dog/Lastdrogz")
+                    ],
+                    [
+                        InlineKeyboardButton("⛺ Hᴏᴍᴇ", callback_data="gotohome"),
+                        InlineKeyboardButton("🗑 Cʟᴏsᴇ", callback_data="closeMessage")
+                    ]
+                ]
+            )
+        )
+
+    elif "help" in cb_data:
+        await cmd.message.edit(
+            Config.HELP_TXT,
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("💸 Dᴏɴᴀᴛᴇ", callback_data="aboutdevs")
+                    ],
+                    [
+                        InlineKeyboardButton("⛺ Hᴏᴍᴇ", callback_data="gotohome"),
+                        InlineKeyboardButton("🗑 Cʟᴏsᴇ", callback_data="closeMessage")
+                    ]
+                ]
+            )
+        )
+
+    elif "gotohome" in cb_data:
+        await cmd.message.edit(
+            Config.HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("♡︎ Cᴏɴᴛᴀᴄᴛ 🧛‍♂️ Aᴅᴍɪɴ ♡︎", url='https://t.me/KR_Admin_Bot')
+                    ],
+                    [
+                        InlineKeyboardButton("👨‍💻 Mʏ Fᴀᴛʜᴇʀ", url="https://t.me/Lastdrogz"),
+                        InlineKeyboardButton("📢 Uᴘᴅᴀᴛᴇ", url="https://t.me/KR_botz")
+                    ],
+                    [
+                        InlineKeyboardButton("📚 Aʙᴏᴜᴛ", callback_data="aboutbot"),
+                        InlineKeyboardButton("💡 Hᴇʟᴘ", callback_data="help")
+                    ]
+                ]
+            )
+        )
