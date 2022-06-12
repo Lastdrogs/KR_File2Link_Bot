5f5f1ff322ba4604b6f92897d7b7e07585526f5c
LᴀsᴛDʀᴏɢᴢ <105853088+Lastdrogz@users.noreply.github.com>
2022-06-12 09:44:43 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -10,7 +10,7 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'KR_File2link_Bot)
+    SESSION_NAME = str(getenv('SESSION_NAME', 'KRFile2linkBot)
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '2419200'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
